ba9deaf819a89ca19a7df4400dba187c8423e11c
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-12-08 19:12:44 -0600

    pull the information for one pokemon

--- js/home.js
+++ js/home.js
@@ -6,14 +6,36 @@
      id:'15',
  }
 
+ //creating url
  const pokemonUrl =`${pokemonData.url}?types=${pokemonData.type}&ids=${pokemonData.id}`
 
 //console.log to see the status of URl
 // console.log(pokemonUrl);
 
 //BY using fetch we will now get all the data needed for pokemon
-
 fetch(pokemonUrl)
-    .then( (info) => console.log(info.json()) )
-    .then( (pokemony) => console.log(pokemony))
+    .then( (info) => (info.json()) )
+    .then( (pokemony) => structure(pokemony))
 
+
+//now after getting all the information we can build the pokemon layout
+
+function structure(info) {
+    console.log(info);
+ const layout = `
+            <div class="card" style="width: 14rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${info.name}</h5>
+                </div>
+                <img src=${info.sprites.front_default} class="card-img-top" alt="...">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"> <span> Height: ${info.height}</span></li>
+                     <li class="list-group-item"> <span> Weight: ${info.weight}</span></li>
+                </ul>
+            </div>   
+ `
+
+    //using DOM to printout all the info in webpage
+    const pokemonInfo = document.querySelector('.pokemon')
+    pokemonInfo.innerHTML = layout;
+}
