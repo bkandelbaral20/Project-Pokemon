d1488d487f5f8649f989067893998a787e5dd9ba
Binjita <binjita.baral@gmail.com>
2020-12-30 11:59:29 -0600

    fetched all the details for pokemon from API

--- js/home.js
+++ js/home.js
@@ -1,71 +1,59 @@
 
 //creating object of what we need from this API
- const pokemonData = {
-     url:'https://pokeapi.co/api/v2',
-     type:'/pokemon',
-     offset:'/?offset=20&limit=20',
- }
+const pokemonData = {
+    url:'https://pokeapi.co/api/v2',
+    type:'/pokemon',
+    offset:'/?offset=20&limit=20',
+}
+//creating url
+const pokemonUrl = pokemonData.url + pokemonData.type + pokemonData.offset
 
- //creating url
-    const pokemonUrl = pokemonData.url + pokemonData.type + pokemonData.offset
 // console.log(pokemonUrl);
 
 //BY using fetch we will now get all the data needed for pokemon
+
 fetch(pokemonUrl)
-    .then( (info) => (info.json()) )
-    .then( (pokemony) => buildPokemon(pokemony))
+    .then( info => info.json())
+    .then(buildPokemon)
     .then(structure)
     .catch(console.error)
 
-//now after getting all the information we can build the pokemon layout
-
- async function buildPokemon(data){
+async function buildPokemon(data){
     const results = data.results;
 
-    //making an array to put all the list of information
-    let listOfPokemon = results.map(async result => {
-        let newPokemon = {
-            name : "",
-            height : 0,
-            weight : 0,
-            sprite : "",
-        }
-        let res = await fetch(result.url)
-            .then(pokemon => pokemon.json());
+   let pokemons = results.map( async result => {
+       const pokeData = await fetch(result.url).then(d => d.json())
+       let newPokemon = {
+           name : pokeData.name,
+           height : pokeData.height,
+           weight : pokeData.weight,
+           sprite : pokeData.sprites.front_default,
+       }
+       return newPokemon
+   })
 
-        newPokemon.name = res.name;
-        newPokemon.height = res.height;
-        newPokemon.weight = res.weight;
-        newPokemon.sprite = res.sprites.front_default;
-
-        // return Promise.resolve(newPokemon);
-        return await newPokemon;
-    });
-         return listOfPokemon;
-     // console.log(listOfPokemon);
+    const listOfPokemon = await Promise.all(pokemons)
+    return listOfPokemon;
 }
 
 function structure(arrayOfPokemon) {
-  let string = "";
-    arrayOfPokemon.forEach(element => {
-         console.log(element.name);
-
-    string += `<div class="card" style="width: 14rem;">
-            <div class="card-body">
-            <h5 class="card-title"><strong>${element.name}</strong></h5>
-        </div>
-          <img src=${element.sprite} class="card-img-top" alt="...">
-
-            <ul class="list-group list-group-flush">
-            <li class="list-group-item"> <span> Height: ${element.height}</span></li>
-        <li class="list-group-item"> <span> Weight: ${element.weight}</span></li>
-        </ul>
-        </div>`;
-
-     });
-
+    const pokemonHTML = arrayOfPokemon.reduce((stringBuilder, element) => stringBuilder.concat(createPokemon(element)), "");
     //using DOM to printout all the info in webpage
     const pokemonInfo = document.querySelector('.pokemon');
-    pokemonInfo.innerHTML = string;
+    pokemonInfo.insertAdjacentHTML("beforeend", pokemonHTML);
 }
 
+function createPokemon(pokemon){
+    return `
+    <div class="card" style="width: 14rem;">
+        <div class="card-body">
+           <h5 class="card-title"><strong>${pokemon.name}</strong></h5>
+        </div>
+        <img src= ${pokemon.sprite} class="card-img-top" alt="...">
+        <button class="fav" type="submit"><i class="fa fa-heart" style="font-size:30px"></i></i></button>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"> <span> Height: ${pokemon.height}</span></li>
+            <li class="list-group-item"> <span> Weight: ${pokemon.weight}</span></li>
+        </ul>
+    </div>`
+}
